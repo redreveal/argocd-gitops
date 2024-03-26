e49b6ecec2c362fea90b5b928182d66de9359de1
Redon Berisha <rberisha@revealdata.com>
2024-03-26 14:11:14 +0100

    DEV-8398

--- scripts/process_values_to_cm.py
+++ scripts/process_values_to_cm.py
@@ -13,7 +13,7 @@ def save_values_file(values, values_file_path):
 
 
 def update_values_with_config(values, config):
-    # Ensure the 'configMap' and 'config_values' keys exist
+    # we will be checking that configMap exists and also the config_values
     if 'configMap' not in values:
         values['configMap'] = {}
     values['configMap']['config_values'] = config
@@ -25,18 +25,13 @@ def process_config_file(config_file_path, values_file_path):
     with open(config_file_path, 'r') as file:
         new_config = yaml.safe_load(file)
 
-    # Load the existing values.yaml
     values = load_values_file(values_file_path)
-
-    # Update values.yaml with the new config
     updated_values = update_values_with_config(values, new_config)
-
-    # Save the updated values.yaml back
     save_values_file(updated_values, values_file_path)
 
 
 if __name__ == "__main__":
-    input_config_file = sys.argv[1]  # The modified processing-agent-config.yml file
-    input_values_file = sys.argv[2]  # The path to the values.yaml to be updated
+    input_config_file = sys.argv[1]  # The file that Elliot's team modifies
+    input_values_file = sys.argv[2]  # The path to the remote repo where the values file is stored
 
     process_config_file(input_config_file, input_values_file)
